8926acb5e438839eb2e5cf14361427e4028aed36
mpouch <177807460+mpouch@users.noreply.github.com>
2024-10-26 17:22:13 -0300

    Validar inputs en login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormGroup, ValidationErrors } from '@angular/forms';
+import { Validators, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required, longitudValida]],
+      password: ['', Validators.required]
+    })
+  }
 
   ngOnInit() {
   }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Formulario enviado', this.loginForm.value);
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
+}
+
+export const longitudValida = (campo: AbstractControl): ValidationErrors | null => {
+  const valor = campo.value;
+  if (valor && (valor.length < 3 || valor.length > 8)) {
+    return {longitudInvalida: true};
+  }
+
+  return null;
+
 
 }
