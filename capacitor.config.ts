9ef9b2ce92a0b71f5bb4d386caafb302b50a2ef1
mpouch <177807460+mpouch@users.noreply.github.com>
2024-11-25 21:53:19 -0300

    Concretar conexión a API REST

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,18 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'skeleton-app',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    androidScheme: 'http',
+    cleartext: true,
+    allowNavigation: [
+      'http://10.0.2.2:3000/*',
+      'http://10.0.2.2/*'
+    ]
+  },
+  android: {
+    allowMixedContent: true
+  }
 };
 
 export default config;
